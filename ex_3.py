bcfca692bb54da77a40d5097b097b4e1c813a50b
makooster <112792508+makooster@users.noreply.github.com>
2023-01-19 18:28:04 +0300

    first commit

---
+++ ex_3.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if int((b**2 + a**2)**0.5) == c :
+    print("c is the hypotenuse of the triangle")
+else:
+    print("c is the hypotenuse of the triangle")
